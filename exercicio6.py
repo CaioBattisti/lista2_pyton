9373d0dee33592d0094005ec61fa72228561940d
CaioBattisti <cluizbattisti@gmail.com>
2025-02-07 19:41:38 -0300

    atualização das atividades

---
+++ exercicio6.py
@@ -0,0 +1,4 @@
+#faça um rograma que solicite o nome e sobrenome do usuario e exiba a mensagem "ola" + nome digitado + sobrenome digitado
+nome = input("digite seu nome: ")
+sobrenome = input("digite seu sobrenome: ")
+print("ola",nome,sobrenome)
