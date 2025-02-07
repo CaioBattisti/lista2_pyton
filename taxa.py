91ab2341457bceec2aab33e3a0936757df717cc5
CaioBattisti <cluizbattisti@gmail.com>
2025-02-07 18:44:41 -0300

    atualização
    Atualizei botando novos codigos

---
+++ taxa.py
@@ -0,0 +1,4 @@
+#faça um programa que receba um salario de 1500 e tenha um aumento de 5% do salario e ao final exiba o salario com o aumento
+salario = 1500
+aumento = 5
+print(salario + (salario * aumento /100))
