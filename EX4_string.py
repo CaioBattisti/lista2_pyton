8780d36756f9ed7f53e449d08892369eea61e591
CaioBattisti <cluizbattisti@gmail.com>
2025-02-10 20:40:13 -0300

    atualizado

---
+++ EX4_string.py
@@ -0,0 +1,7 @@
+email = 'caiobattisti@gmail.com'
+
+print("\nPegando posição ZERO no email atribuido:")
+print(email[0])
+
+print("\npegando da posição ZERO ate a ONZE do email atribuido:")
+print(email[0:13])
