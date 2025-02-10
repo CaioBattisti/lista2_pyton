e28c9cbb63d097b6ab27af1df7f563f391d2d943
CaioBattisti <cluizbattisti@gmail.com>
2025-02-10 20:43:02 -0300

    atuaização

--- EX4_string.py
+++ EX4_string.py
@@ -5,3 +5,8 @@ print(email[0])
 
 print("\npegando da posição ZERO ate a ONZE do email atribuido:")
 print(email[0:13])
+
+email = 'caiobattisti@gmail.com'
+
+print("\nusando o metodo LEN para saber o tamanho da variavel email:")
+print(len(email))
