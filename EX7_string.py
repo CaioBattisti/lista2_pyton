c169bdc197bc52bdf67f61e7961f1ebd6592c66d
CaioBattisti <cluizbattisti@gmail.com>
2025-02-10 21:16:33 -0300

    atualização

--- EX7_string.py
+++ EX7_string.py
@@ -2,3 +2,8 @@ faturamento = 2000
 custo = 500
 lucro = faturamento - custo
 print("O faturamento da loja foi de: %s" %faturamento)
+
+faturamento = 2000
+custo = 500
+lucro = faturamento - custo
+print("O faturamento da loja foi de: %d" % faturamento)
