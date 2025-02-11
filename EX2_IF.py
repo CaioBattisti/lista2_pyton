0bd8ce801ed6afb1c76da41b471ef3f33eb93bed
CaioBattisti <cluizbattisti@gmail.com>
2025-02-11 20:59:03 -0300

    atualização

--- EX2_IF.py
+++ EX2_IF.py
@@ -1,9 +1,10 @@
 meta = 20000
-vendas =int(input(digite a venda:))
+vendas = 1000
 
 if vendas < meta:
     print("não ganhou bônus")
 elif vendas > (meta*2):
+    bonus = 0.07 * vendas
     print("ganhou {} de bônus".format(bonus))
 else:
     bonus = 0.03 * vendas
---
+++ EX9_IF.py
@@ -0,0 +1,4 @@
+if '@' in 'caioluizbattisti@gmail.com':
+    print('o email e valido')
+else:
+    print('o email nao e valido')
