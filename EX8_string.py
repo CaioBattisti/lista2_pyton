28cebca79b5ef84d7344b614f584f36fcc9b3f78
CaioBattisti <cluizbattisti@gmail.com>
2025-02-10 21:41:03 -0300

    atualização

--- EX8_string.py
+++ EX8_string.py
@@ -1,9 +1,18 @@
-#transforma apenas a primeira letra de uma string em maiscula
+#transforma apenas a primeira letra de uma string em maiscula.
 nome = "caio"
 print(nome.capitalize(),"\n")
-#transorma todas as letras em minusculas
+#transorma todas as letras em minusculas.
 nome = "CAIO"
 print(nome.casefold(),"\n")
-#conta um numero de vezes que um caractere especifico aparece na string
+#conta um numero de vezes que um caractere especifico aparece na string.
 nome = "CaioLuizBattisti@gmail.com"
 print(nome.count('i'),"\n")
+#retorna true (verdadeiro) ou false (falso) para um teste se a string com uma string especifica.
+nome = "CaioLuizBattisti@gmail.com"
+print(nome.endswith('gmail.com'),"\n")
+#esncontra a posição do termo procurado, lembre-se que se começa com ZERO.
+nome = "CaioLuizBattisti@gmail.com"
+print(nome.find('@'),"\n")
+#verifica se o texto e todo feito com letras.
+nome = "Caio"
+print(nome.isalpha(),"\n")
