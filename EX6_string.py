a90ee0901b0b4ca0736cc46021755cd16ef25938
CaioBattisti <cluizbattisti@gmail.com>
2025-02-10 21:09:46 -0300

    atualização

--- EX6_string.py
+++ EX6_string.py
@@ -1,4 +1,4 @@
 faturamento = 2000
 custo = 500
 lucro = faturamento - custo
-print("O faturamento da loja foi de: {} O custo foi de {} e o lucro foi de {}".format(faturamento,custo,lucro))
+print("O faturamento da loja foi de: {0} O custo foi de {1} e o lucro foi de {2}".format(faturamento,custo,lucro))
