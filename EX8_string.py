7bcacf7e4d0b1fa87b2c35af2697b2e9225a40c4
CaioBattisti <cluizbattisti@gmail.com>
2025-02-10 21:29:11 -0300

    atualização

--- EX8_string.py
+++ EX8_string.py
@@ -1,3 +1,6 @@
 #transforma apenas a primeira letra de uma string em maiscula
 nome = "caio"
 print(nome.capitalize(),"\n")
+#transorma todas as letras em minusculas
+nome = "CAIO"
+print(nome.casefold(),"\n")
