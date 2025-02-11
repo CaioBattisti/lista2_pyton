4c0146896e687d6100804c89df223d93049c462e
CaioBattisti <cluizbattisti@gmail.com>
2025-02-10 21:32:31 -0300

    atualização

--- EX8_string.py
+++ EX8_string.py
@@ -4,3 +4,6 @@ print(nome.capitalize(),"\n")
 #transorma todas as letras em minusculas
 nome = "CAIO"
 print(nome.casefold(),"\n")
+#conta um numero de vezes que um caractere especifico aparece na string
+nome = "CaioLuizBattisti@gmail.com"
+print(nome.count('i'),"\n")
