5e8633eba600dde3dacb1315e4ad363372d5cf95
CaioBattisti <cluizbattisti@gmail.com>
2025-02-17 20:46:38 -0300

    atualização

--- EX4_FOR.py
+++ EX4_FOR.py
@@ -1,6 +1,6 @@
 estoque = (1200,300,800,1500,1900,2750,400,20,23,70,90,80,1100)
 produtos =("coca cola","pepsi","guarana","skol","brahma","agua","del vale","red bull","dolly","fanta","sptrite","sukita","pepsi twist")
-nivel_minimo = 20000
+nivel_minimo = 50
 for i,qtde in enumerate(estoque):
     if qtde < nivel_minimo:
         print(produtos[i],qtde)
