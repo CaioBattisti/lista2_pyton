d83b6a063ebab728ec1984dae1d933be30519585
CaioBattisti <cluizbattisti@gmail.com>
2025-02-17 20:55:56 -0300

    atualização

---
+++ EX5_for_BREAK.py
@@ -0,0 +1,9 @@
+pessoas_presentes = ['john snow','jesse pinkman','aria stark','tyrion','lannister']
+#quero saber se uma pessoa espeifica esta presente .
+chamada = 'aria stark'
+for pessoa in pessoas_presentes:
+    if pessoa == chamada:
+        print('{} esta presente.'.format(chamada))
+        break
+    else:
+        print("so um print para mostrar que o for passou por essa pessoa: "+str(pessoa))
