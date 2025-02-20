c32970c8045f1508a6837982482967e2fac0fe65
CaioBattisti <cluizbattisti@gmail.com>
2025-02-20 19:51:43 -0300

    atualização

---
+++ EX6_dicionario_for.py
@@ -0,0 +1,5 @@
+vendas_tecnologia = {'iphone':'1500','sansung galaxy':'12000','Tv samsung':'10000','ps5':'14300'}
+#demonsrando for
+for chave in vendas_tecnologia:
+    print(chave)#ele nao diz o valor
+    print("o produto {} vendeu {} unidades.".format(chave,vendas_tecnologia[chave]))#diz o valor
