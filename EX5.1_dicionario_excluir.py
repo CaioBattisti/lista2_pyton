d2feb6f0d74ae2f559209c99954307e75c434d59
CaioBattisti <cluizbattisti@gmail.com>
2025-02-20 19:40:55 -0300

    atualização

---
+++ EX5.1_dicionario_excluir.py
@@ -0,0 +1,5 @@
+lucro_jun ={"janeiro":1,"fevereiro":2,"marco":3}
+lucro_2tri ={"abril":4,"maio":5,"junho":6}
+lucro_jun=lucro_2tri.pop("junho")
+print(lucro_2tri)
+print(lucro_jun)
--- EX5_dicionario_excluir.py
+++ EX5_dicionario_excluir.py
@@ -1,4 +1,4 @@
-#godigo de excluir itens
+#godigo de excluir itens usando del
 lucro_1tri ={"janeiro":1,"fevereiro":2,"marco":3}
 lucro_2tri ={"abril":4,"maio":5,"junho":6}
 del lucro_2tri["junho"]
