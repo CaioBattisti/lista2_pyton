37003f5e7dd7a17659e2b4b7f55ffabfb70d2922
CaioBattisti <cluizbattisti@gmail.com>
2025-02-20 18:56:22 -0300

    atualização

--- EX1_Tulpa.py
+++ EX1_Tulpa.py
@@ -1,3 +1,4 @@
+#lista e em colchetes e a tulpa em parenteses:
 vendas =("Caio","25/08/2020","02/10/2008",2000,"estagiario")
 nome, data_contratacao, data_nascimento, salario, cargo = vendas
 print(nome)
