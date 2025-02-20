e08d09c9577f0d5b58d4dd34e2accf299e547a22
CaioBattisti <cluizbattisti@gmail.com>
2025-02-20 19:44:59 -0300

    atualização

--- EX5.1_dicionario_excluir.py
+++ EX5.1_dicionario_excluir.py
@@ -1,5 +1,9 @@
+#excluindo da lista usando .pop obs:mais deixa o valor
 lucro_jun ={"janeiro":1,"fevereiro":2,"marco":3}
 lucro_2tri ={"abril":4,"maio":5,"junho":6}
 lucro_jun=lucro_2tri.pop("junho")
 print(lucro_2tri)
 print(lucro_jun)
+#REMOVENDO TDOS OS DADOS DE UM DICIONARIO:usando .clear obs:apaga todos os dados
+lucro_2tri.clear()
+print(lucro_2tri)
