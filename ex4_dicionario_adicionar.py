5c98539b1b922bb2fe9c39b8a9ff5f438a6271dd
CaioBattisti <cluizbattisti@gmail.com>
2025-02-20 19:29:25 -0300

    atualização

--- ex4_dicionario_adicionar.py
+++ ex4_dicionario_adicionar.py
@@ -3,6 +3,6 @@ vendas_vendedoras = {}
 vendas_vendedoras["joao"] = 1500
 print(vendas_vendedoras)
 
-vendas_vendedoras.update({"lira":50,"nathalie":100})
+vendas_vendedoras.update({"lira":50,"nathalie":100})#adicionando
 print(vendas_vendedoras)
 print("fim do programa")
