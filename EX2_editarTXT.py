021e760c667a2775f45bec7ddbb04985117e2e06
CaioBattisti <cluizbattisti@gmail.com>
2025-02-21 19:41:49 -0300

    atualização

---
+++ EX2_editarTXT.py
@@ -0,0 +1,8 @@
+arquivo2 =open('C:/Users/caio_battisti/Documents/TXT/GOT.txt','w')
+arquivo2.write("Escrevendo no arquivo\n")
+arquivo2.write("John Snow\n")
+arquivo2.write("Arya Stark\n")
+arquivo2.write("Sansa Stark\n")
+arquivo2.write("bran Stark\n")
+arquivo2.close()
+print("fim do programa")
---
+++ EX3_listaCompra.py
@@ -0,0 +1,3 @@
+arquivo3 =open('C:/Users/caio_battisti/Documents/TXT/lista_compra.txt','r')
+linhas = arquivo3.readlines()
+print(linhas)
